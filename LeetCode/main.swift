4e5863b72aee8a08751f2c6544bd4704236bfb85
wangyang <wangyang@wanmeizhensuo.com>
2018-04-05 19:27:02 +0800

    Initial Commit

---
+++ LeetCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetCode
+//
+//  Created by wangyang on 2018/4/5.
+//  Copyright © 2018年 北京更美互动信息科技有限公司. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
